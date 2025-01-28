02807c3bdeea2f726bc608ad375dcc887866efe7
Maksim Tsikvasvili <maksimtsitkool@gmail.com>
2025-01-28 15:16:37 +0200

    Update opilane.sql

--- opilane.sql
+++ opilane.sql
@@ -63,3 +63,30 @@ delete from opilane where opilaneId=5;
 --Andmete uuendamine
 update opilane set aadress='Tartu'
 where opilaneId=3
+
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint
+); 
+select * from Language;
+
+insert into Language(ID,Code,Language)
+values (4,'DEU','saksa')
+
+create table keeleValik(
+keeleValikud int primary key identity(1,1),
+valikuNimetus varchar(10) not null,
+opilaneId int,
+foreign key (opilaneId) references opilane(opilaneId),
+Language int,
+foreign key (Language) references Language(ID)
+)
+select * from keeleValik;
+select * from Language;
+select * from opilane;
+insert into keeleValik(valikuNimetus,opilaneId,Language)
+values ('Valik B',2,3)
